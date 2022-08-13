e47f6f0fb038bfac60ff46020ff80dac4b19cb2b
RonaldoFagundes <ronaldofagundes@gmail.com>
2022-08-13 19:32:02 -0300

    to up repository

---
+++ js/es.js
@@ -0,0 +1,62 @@
+
+  let projectFront = document.querySelector(".projects-front");
+  let projectBack = document.querySelector(".projects-back");
+  let projectMobilie = document.querySelector(".projects-mobilie");
+
+
+   showProject =(project)=>{
+
+       if(project == "front"){
+         alert("frontEnd");
+         projectFront.classList.add("projects-front-show");
+         projectBack.classList.remove("projects-back-show");
+         projectMobilie.classList.remove("projects-mobilie-show");
+         btn_front.classList.add("active-btn");
+         btn_back.classList.remove("active-btn");
+         btn_mobilie.classList.remove("active-btn");
+        }
+       else if (project == "back"){
+        alert("backend");
+        projectBack.classList.add("projects-back-show");
+        projectFront.classList.remove("projects-front-show");
+        projectMobilie.classList.remove("projects-mobilie-show");
+        btn_front.classList.remove("active-btn");
+        btn_back.classList.add("active-btn");
+        btn_mobilie.classList.remove("active-btn");
+       }
+       else if (project == "mobilie"){
+        alert("mobilie"); 
+        projectMobilie.classList.add("projects-mobilie-show");
+        projectFront.classList.remove("projects-front-show");
+        projectBack.classList.remove("projects-back-show");
+        btn_front.classList.remove("active-btn");
+        btn_back.classList.remove("active-btn");
+        btn_mobilie.classList.add("active-btn");
+       }
+  }
+
+   let btn_front = document.querySelector("#btn-front");
+   let btn_back = document.querySelector("#btn-back");
+   let btn_mobilie = document.querySelector("#btn-mobilie");
+
+   
+
+   btn_front.addEventListener("click",
+   ()=>{
+        showProject("front");
+   });
+
+
+   btn_back.addEventListener("click",
+   ()=>{
+        showProject("back");
+   });
+
+   btn_mobilie.addEventListener("click",
+   ()=>{
+        showProject("mobilie");
+   });
+       
+   
+
+      
